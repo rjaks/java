3a432b3d21d053c79c1664d9695dbf0a2bba505a
Adrian <107988407+rjaks@users.noreply.github.com>
2024-09-26 16:23:26 +0800

    Issue: Output Error

---
+++ Midterms/SeatPlanMakerV2/SeatPlanMaker2.java
@@ -0,0 +1,75 @@
+package Midterms.SeatPlanMakerV2;
+
+import java.util.*;
+
+public class SeatPlanMaker2 {
+    static int r, rc, lc, seatcount = 1, reservedseat;
+    static int[][] seats;
+    static Scanner s = new Scanner(System.in);
+
+    static void generateSeats(){
+        seats = new int[r][rc + lc];
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < lc; j++) {
+                seats[i][j] = seatcount;
+                seatcount++;
+            }
+            for (int k = lc; k < rc + lc; k++) {
+                seats[i][k] = seatcount;
+                seatcount++;
+            }
+        }
+    }
+
+    static void display(){
+        for (int j = 0; j < r; j++){
+            for (int k = 0; k < lc; k++) {
+                if (seats[j][k] == 0) System.out.print("[XX]");
+                else System.out.printf("[%02d]", seats[j][k]);
+            }
+            System.out.print('\t');
+            for (int l = lc; l < rc + lc; l++) {
+                if (seats[j][l] == 0) System.out.print("[XX]");
+                else System.out.printf("[%02d]", seats[j][l]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void reserve(int n){
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < lc + rc; j++){
+                if (n == seats[i][j]) {
+                    if (seats[i][j] == 0) {
+                        System.out.println("SEAT TAKEN.");
+                        break;
+                    }
+                    else { 
+                        seats[i][j] = 0;
+                        display();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        do{
+            if (r * (lc + rc) > 99) System.out.println("Total number of seats should not exceed 99.");
+            System.out.print("Enter row#, left column#, and right column#, separated by space: ");
+            r = s.nextInt();
+            lc = s.nextInt();
+            rc = s.nextInt();
+        }
+        while (r * (lc + rc) > 99);
+        generateSeats();
+        display();
+        do{
+            System.out.println();
+            System.out.print("Select seat number to take <Enter 0 to quit>: ");
+            reservedseat = s.nextInt();
+            reserve(reservedseat);
+        } while (reservedseat != 0);
+    }
+}
