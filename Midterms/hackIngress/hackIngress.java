1ac31eceeb4af9fb39d1a2ebd2d5313d8ef880bf
Adrian <107988407+rjaks@users.noreply.github.com>
2024-10-01 04:06:47 +0800

    Update: New File Revision

--- Midterms/hackIngress/hackIngress.java
+++ Midterms/hackIngress/hackIngress.java
@@ -31,7 +31,14 @@ public class hackIngress{
         // 23 0600 0659 0700 0715 0719 0959 1000 1004 1030 1500 1505 1510 1512 1516 1521 1900 1831 1832 1948 2359 0000 0004 0430
         // 16 600 659 715 719 | 959 1004 1030 | 1500 1505 1510 1516 | 1900 1948 | 2359 0004 | 0430
         else if (count > 0 && cycle[0] > 1200 && time < 1200 ){
-            if ((2400 - cycle[count-1]) + time >= 400 + first){
+            if ((2400 - cycle[count-1]) + time >= 5){
+                cycle[count] = time;
+                System.out.printf("(%d)", cycle[count-1]);
+                System.out.print(time + " ");
+                count++;
+                success++;
+            }
+            else if ((2400 - cycle[count-1]) + time >= 400 + first){
                 cycle = new int[4];
                 cycle[0] = time;
                 System.out.print(" | " + time + " ");
@@ -39,17 +46,11 @@ public class hackIngress{
                 count++;
                 success++;
             }
-            else if ((2400 - cycle[count-1]) + time >= 5){
-                cycle[count] = time;
-                System.out.printf("(%d)", cycle[count-1]);
-                System.out.print(time + " ");
-                count++;
-                success++;
-            }
         }
         else if (count > 0 && count < 4 && time - cycle[count-1] >= 5){ // checks if the hack is successful or within the cycle
             cycle[count] = time;
             System.out.printf("(%d)", cycle[count-1]);
+            System.out.printf("[%d]", time - cycle[count-1]);
             System.out.print(time + " ");
             count++;
             success++;
